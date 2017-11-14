7187f4e566195b3253a3c70a3323d0c5f4a047aa
Nikita Mynych <nekithp7@gmail.com>
2017-11-14 21:14:17 +0200

    initial database setup

---
+++ server/api/Features/Shared/DBContext.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Driver;
+
+namespace api.Features.Shared
+{
+	public class DBContext
+	{
+		private IMongoDatabase database;
+		public string ConnectionString { get; set; }
+		public string DBName { get; set; }
+
+		public IMongoDatabase Database
+		{
+			get
+			{
+				if (database == null)
+				{
+					var client = new MongoClient(ConnectionString);
+					database = client?.GetDatabase(DBName);
+				}
+
+				return database;
+			}
+		}
+	}
+}
