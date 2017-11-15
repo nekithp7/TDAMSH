f2a74c25063f90cb7642d1813e30dc87400c20f3
Inna Bezzub <inna_bezzub@epam.com>
2017-11-15 13:25:53 +0200

    remove unnecessary packages

--- ui/config/webpack.common.js
+++ ui/config/webpack.common.js
@@ -1,7 +1,6 @@
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 var ExtractTextPlugin = require('extract-text-webpack-plugin');
-var TypedocWebpackPlugin = require('typedoc-webpack-plugin');
 var helpers = require('./helpers');
 
 module.exports = {
