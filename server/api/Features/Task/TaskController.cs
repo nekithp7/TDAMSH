3304195294ea46b5bed608db86ae8d0f30baf098
Nikita Mynych <nekithp7@gmail.com>
2017-11-28 20:19:48 +0200

    initial taskController implementation

--- server/api/Features/Task/TaskController.cs
+++ server/api/Features/Task/TaskController.cs
@@ -14,5 +14,47 @@ namespace api.Features.Task
 		private readonly ITaskService taskService;
 
 		public TaskController(ITaskService taskService) => this.taskService = taskService;
+
+		string Id => User?.FindFirstValue(ClaimTypes.Sid);
+
+		[HttpGet("all")]
+		public ActionResult GetAllTasks()
+		{
+			var result = taskService.GetAllTasks(Id);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpGet]
+		public ActionResult GetTask([FromQuery]string taskId)
+		{
+			var result = taskService.GetTask(Id, taskId);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpPost]
+		public ActionResult AddTask([FromBody]TaskModel model)
+		{
+			var result = taskService.AddTask(Id, model);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpPut]
+		public ActionResult UpdateTask([FromBody]TaskModel model)
+		{
+			var result = taskService.UpdateTask(Id, model);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpDelete]
+		public ActionResult DeleteTask([FromQuery]string taskId)
+		{
+			var result = taskService.DeleteTask(Id, taskId);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
 	}
 }
