82fa061f67fc9d487db8a74b265f0dcdc2c3c268
nekithp7 <nekithp7@gmail.com>
2017-12-04 21:21:54 +0200

    Tasks initial model (#9)
    * initial structure for tasks handling
    
    * initial taskRepository implementation
    
    * initial taskService implementation
    
    * initial taskController implementation

---
+++ server/api/Features/Shared/Task/TaskContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Options;
+
+using MongoDB.Driver;
+
+using api.Features.Task;
+
+namespace api.Features.Shared.Task
+{
+	public class TaskContext
+	{
+		private IMongoDatabase database;
+
+		public TaskContext(IOptions<DBContext> dbContext) => database = dbContext.Value.Database;
+
+		public IMongoCollection<TaskModel> Tasks => database.GetCollection<TaskModel>("tasks");
+	}
+}
---
+++ server/api/Features/Task/TaskController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+using api.Features.Task.Service;
+
+namespace api.Features.Task
+{
+	[Produces("application/json")]
+	[Route("[controller]")]
+	[Authorize]
+	public class TaskController : Controller
+	{
+		private readonly ITaskService taskService;
+
+		public TaskController(ITaskService taskService) => this.taskService = taskService;
+
+		string Id => User?.FindFirstValue(ClaimTypes.Sid);
+
+		[HttpGet("all")]
+		public ActionResult GetAllTasks()
+		{
+			var result = taskService.GetAllTasks(Id);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpGet]
+		public ActionResult GetTask([FromQuery]string taskId)
+		{
+			var result = taskService.GetTask(Id, taskId);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpPost]
+		public ActionResult AddTask([FromBody]TaskModel model)
+		{
+			var result = taskService.AddTask(Id, model);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpPut]
+		public ActionResult UpdateTask([FromBody]TaskModel model)
+		{
+			var result = taskService.UpdateTask(Id, model);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+
+		[HttpDelete]
+		public ActionResult DeleteTask([FromQuery]string taskId)
+		{
+			var result = taskService.DeleteTask(Id, taskId);
+
+			return StatusCode(result.StatusCode, result.Message);
+		}
+	}
+}
